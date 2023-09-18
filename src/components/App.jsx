864c2eace4eb446a854f6eec64e2723f729d52b2
ewasawczynska <sawczynska.e@gmail.com>
2023-09-18 16:48:09 +0200

    API service

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,5 @@
+import { SearchBar } from 'components';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  <SearchBar />;
 };
